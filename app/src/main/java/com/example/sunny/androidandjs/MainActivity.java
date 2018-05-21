ca5875e2e097821307823ff18ce0ef1b65e1970f
yangmtou <yangmtou@163.com>
2018-05-22 00:36:39 +0800

    First commit.

--- app/src/main/java/com/example/sunny/androidandjs/MainActivity.java
+++ app/src/main/java/com/example/sunny/androidandjs/MainActivity.java
@@ -3,13 +3,18 @@ package com.example.sunny.androidandjs;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements YctJsBridge {
+public class MainActivity extends AppCompatActivity implements YctJsBridge,View.OnClickListener {
 
     private WebView layout_main_wv;
     private TextView layout_main_tv;
+    private EditText layout_main_et;
+    private Button layout_main_btn_send;
     private Handler handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +26,10 @@ public class MainActivity extends AppCompatActivity implements YctJsBridge {
     private void initWidgets(Bundle savedInstanceState) {
         layout_main_wv = findViewById(R.id.layout_main_wv);
         layout_main_tv = findViewById(R.id.layout_main_tv);
+
+        layout_main_et = findViewById(R.id.layout_main_et);
+        layout_main_btn_send = findViewById(R.id.layout_main_btn_send);
+        layout_main_btn_send.setOnClickListener(this);
 
         layout_main_wv.getSettings().setJavaScriptEnabled(true);
         layout_main_wv.addJavascriptInterface(new YctJsInterface(this),"yctjsinterface");
@@ -37,5 +46,14 @@ public class MainActivity extends AppCompatActivity implements YctJsBridge {
                 layout_main_tv.setText(str);
             }
         });
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == layout_main_btn_send){
+            String str = layout_main_et.getText().toString();
+            String forJSString = "javascript:if(window.fromAndroid){window.fromAndroid('"+str+"')}";
+            layout_main_wv.loadUrl(forJSString);
+        }
     }
 }
